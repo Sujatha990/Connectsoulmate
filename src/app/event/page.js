fd54883936fa4ea7ca10b253472d9e91baf59714
Sujatha990 <111685640+Sujatha990@users.noreply.github.com>
2024-08-01 13:41:34 +0530

    commit first

--- src/app/event/page.js
+++ src/app/event/page.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import Event from '../Event'
-<<<<<<< HEAD
 import NavBar from '../NavBar'
 import NavBar2 from '../NavBar2'
-=======
->>>>>>> ff833fc005719691db1210e591046a4659dd9126
 
 const page = () => {
   return (
