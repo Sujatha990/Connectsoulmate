44508cf25e49572d320c3fd758bc48050d7153b9
Sujatha990 <111685640+Sujatha990@users.noreply.github.com>
2024-08-01 16:13:42 +0530

    commit first

--- src/app/About.js
+++ src/app/About.js
@@ -10,7 +10,7 @@ const About = () => {
           <div>
             <h2 className="display-4" style={{ paddingLeft: "50px" }}>About Us</h2>
             <p style={{ paddingLeft: "50px" }}>
-              In the rich tapestry of Indian culture, the traditional practice of arranged marriage holds a significant place. For generations, parents have taken on the important task of seeking ideal life partners for their children, collaborating with relatives, marriage bureaus, friends, and various intermediaries. However, the landscape has evolved dramatically in contemporary times. Presently, individuals seeking marriage wish to establish a genuine connection with their future partners, aiming to comprehensively understand and resonate with them before embarking on a lifelong journey together. This shift has sparked a paradigm change, placing Matrimonial Sites in India at the forefront of everyone's attention.
+              In the rich tapestry of Indian culture, the traditional practice of arranged marriage holds a significant place. For generations, parents have taken on the important task of seeking ideal life partners for their children, collaborating with relatives, marriage bureaus, friends, and various intermediaries. However, the landscape has evolved dramatically in contemporary times. Presently, individuals seeking marriage wish to establish a genuine connection with their future partners, aiming to comprehensively understand and resonate with them before embarking on a lifelong journey together. This shift has sparked a paradigm change, placing Matrimonial Sites in India at the forefront of everyone&apos;s attention.
             </p>
             <div style={{ paddingLeft: "50px" }}>
               <Link href="/about1" className="btn btn-primary">More</Link>
