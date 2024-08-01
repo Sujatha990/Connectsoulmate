068725632a58871e80bd08e41bc27728830217f6
Sujatha990 <111685640+Sujatha990@users.noreply.github.com>
2024-08-01 17:01:40 +0530

    commit first

--- src/app/advancesearchdisplaypage/page.js
+++ src/app/advancesearchdisplaypage/page.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import Advanceserchdisplaypage from '../Advanceserchdisplaypage'
-const page = () => {
+import React from 'react';
+import Advanceserchdisplaypage from '../Advanceserchdisplaypage';
+
+const Page = () => {
   return (
     <div>
-      <Advanceserchdisplaypage/>
+      <Advanceserchdisplaypage />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
